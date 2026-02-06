da897e0b2a6f751f9ff0e6b3e5f6f133a4aaf580
Josh Bronson <josh@bronson-lsst.work>
2026-02-05 18:50:40 -0600

    remove location constraint for aws s3

--- scripts/create_kubernetes_cluster_aws.sh
+++ scripts/create_kubernetes_cluster_aws.sh
@@ -13,12 +13,7 @@ set -x
 if aws s3api head-bucket --bucket "$bucket_name" &>/dev/null; then
   echo "| OK. S3 bucket ${bucket_name} exists."
 else
-  if [ "$region" = "us-east-1" ]; then
-      extra_args=()
-  else
-      extra_args=(--create-bucket-configuration LocationConstraint="$region")
-  fi
-  aws s3api create-bucket --bucket "$bucket_name" --acl private "${extra_args[@]}" | cat
+  aws s3api create-bucket --bucket "$bucket_name" --acl private | cat
 fi
 
 aws s3api put-bucket-cors	\
